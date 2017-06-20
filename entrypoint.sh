bbea469424a69309518eef100358bd9e4acb61e5
Eric Carter <eric.carter@us.imshealth.com>
2017-06-19 18:56:11 -0700

    Fixing server reload behaviour

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 savePath=/var/factorio/saves
 saveFile=$savePath/${1:-default}.zip
-if [ ! -d savePath ]; then
-    echo "======================"
-    echo "Creating new save game"
-    echo "======================"
-    factorio --create $saveFile --config /etc/factorio/config
-fi
 
 if [ -f /etc/factorio/map-gen-settings.json ]; then
     map_gen_settings='--map-gen-settings=/etc/factorio/map-gen-settings.json'
@@ -20,9 +14,24 @@ if [ -d /usr/share/factorio/mods ]; then
     mod_directory='--mod-directory=/usr/share/factorio/mods'
 fi
 
-echo "================="
-echo "Starting Factorio"
-echo "================="
-factorio --start-server $saveFile \
-    --config /etc/factorio/config \
-    ${mod_directory} ${map_gen_settings} ${server_settings}
+if [ ! -d savePath ]; then
+    echo "======================"
+    echo "Creating new save game"
+    echo "======================"
+    factorio --create $saveFile --config /etc/factorio/config
+
+	echo "================="
+	echo "Starting Factorio"
+	echo "================="
+	factorio --start-server $saveFile \
+		--config /etc/factorio/config \
+		${mod_directory} ${map_gen_settings} ${server_settings}
+else
+	echo "================="
+	echo "Starting Factorio"
+	echo "================="
+	factorio --start-server-load-latest \
+		--config /etc/factorio/config \
+		${mod_directory} ${map_gen_settings} ${server_settings}
+fi
+
