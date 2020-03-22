ff0e97138261c7f1d9c13183a95de1edb5183b06
Eric Carter <eric.carter@cryowatt.com>
2020-03-21 20:39:41 -0700

    Fixing deploy script

--- Deploy.ps1
+++ Deploy.ps1
@@ -7,12 +7,11 @@ param(
 )
 
 $DeployParameters = @{
-    "saves_storage_account_name" = $StorageAccountName
-    "saves_storage_account_key" = Get-AzStorageAccountKey -Name $StorageAccountName -ResourceGroupName $ResourceGroupName | Select-Object -First 1 -ExpandProperty Value
-    "log_analytics_workspace_id" = Get-AzOperationalInsightsWorkspace -ResourceGroupName $ResourceGroupName -Name $LogWorkspaceName | Select-Object -ExpandProperty CustomerId
-    "log_analytics_workspace_key" = Get-AzOperationalInsightsWorkspaceSharedKeys -ResourceGroupName $ResourceGroupName -Name $LogWorkspaceName | Select-Object -ExpandProperty PrimarySharedKey
-    "factorio_version" = ./Get-LatestVersion.ps1 -FactorioVersion $FactorioVersion
+    saves_storage_account_name = $StorageAccountName
+    saves_storage_account_key = [string] (Get-AzStorageAccountKey -Name $StorageAccountName -ResourceGroupName $ResourceGroupName | Select-Object -First 1 -ExpandProperty Value)
+    log_analytics_workspace_id = [string] (Get-AzOperationalInsightsWorkspace -ResourceGroupName $ResourceGroupName -Name $LogWorkspaceName | Select-Object -ExpandProperty CustomerId)
+    log_analytics_workspace_key = [string] (Get-AzOperationalInsightsWorkspaceSharedKeys -ResourceGroupName $ResourceGroupName -Name $LogWorkspaceName | Select-Object -ExpandProperty PrimarySharedKey)
+    factorio_version = [string] (./Get-LatestVersion.ps1 -FactorioVersion $FactorioVersion)
 }
 
-return $DeployParameters
-#New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateParameterObject $DeployParameters -TemplateFile .\deploy\template.json -Name Hahntorio
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateParameterObject $DeployParameters -TemplateFile .\deploy\template.json -Name Hahntorio
